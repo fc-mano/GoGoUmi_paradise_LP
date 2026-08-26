889534084da235e281d9c3e88dac36bdcc99c821
owl.yuuta <yuutasv@gmail.com>
2026-08-26 14:42:33 +0900

    update: 手動修正（アサイー見出し文言調整・注記文言調整）の反映

--- prices.js
+++ prices.js
@@ -232,8 +232,8 @@ const PRICE_CONFIG = {
   // 9. 料金セクションの注記
   // ----------------------------------------------------------------------------
   notesHtml: `
-    ※ 料金はすべて税込価格です。<BR>商品の受け渡しはカウンターにてお願いいたします。<br>
-    ※ BBQセットやご予約に関するご相談は <a href="https://www.instagram.com/gogoumi_paradise" target="_blank" rel="noopener" style="color:var(--blue);font-weight:700">Instagram DM</a> またはお電話（現地担当：080-4999-0246）にて承ります。
+    ※ 料金はすべて税込価格です。<BR> ※ 商品の受け渡しはカウンターにてお願いいたします。<br>
+    ※ BBQセットやご予約に関するご相談は <a href="https://www.instagram.com/gogoumi_paradise" target="_blank" rel="noopener" style="color:var(--blue);font-weight:700">Instagram DM</a> またはお電話（現地担当　080-4999-0246）にて承ります。
   `
 };
 
